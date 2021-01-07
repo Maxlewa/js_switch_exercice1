d521d03a2e1f006006f0e0a25fad2a59aa67b999
Maxence <lemairemaxence@hotmail.com>
2021-01-07 14:59:21 +0100

    exo ok

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,45 @@
 // EXO 1
 
-// let jour = prompt("Jour de la semaine ?")
+let jour = prompt("Jour de la semaine ?").toLowerCase()
 
-// switch (jour) {
-//     case "lundi":
-//         alert("Tu as sport le lundi")
-//         break;
-//     case "mardi":
-//         alert("Tu as dessin le mardi")
-//         break;
-//     case "mercredi":
-//         alert("Tu as congé le mercredi")
-//         break;
-//     case "jeudi":
-//         alert("Tu as natation le jeudi")
-//         break;
-//     case "vendredi":
-//         alert("Tu as rdv au bar le vendredi")
-//         break;
-//     case "samedi":
-//         alert("Tu as une grosse sieste le samedi")
-//         break;
-//     case "dimanche":
-//         alert("Tu as athlétisme le dimanche")
-//         break;
-//     default:
-//         break;
-// }
+switch (jour) {
+    case "lundi":
+        alert("Tu as sport le lundi")
+        break;
+    case "mardi":
+        alert("Tu as dessin le mardi")
+        break;
+    case "mercredi":
+        alert("Tu as congé le mercredi")
+        break;
+    case "jeudi":
+        alert("Tu as natation le jeudi")
+        break;
+    case "vendredi":
+        alert("Tu as rdv au bar le vendredi")
+        break;
+    case "samedi":
+        alert("Tu as une grosse sieste le samedi")
+        break;
+    case "dimanche":
+        alert("Tu as athlétisme le dimanche")
+        break;
+    default:
+        break;
+}
 
 // EXO 2
 
 let age = parseInt(prompt("Quel est ton âge ?"))
 
-switch (true) {
-    case (age >= 18):
+switch (age >= 18) {
+    case true :
         alert("Tu es majeur")
         break;
-    default:
+    case false :
         alert("Tu es mineur")
+        break;
+    default:
+        alert("error")
         break;
 }
