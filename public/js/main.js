8de6a952eeae9565e8c5c68a24cad734a8fbdcf4
Maxence <lemairemaxence@hotmail.com>
2021-01-07 12:08:38 +0100

    exo ok

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+// EXO 1
+
+// let jour = prompt("Jour de la semaine ?")
+
+// switch (jour) {
+//     case "lundi":
+//         alert("Tu as sport le lundi")
+//         break;
+//     case "mardi":
+//         alert("Tu as dessin le mardi")
+//         break;
+//     case "mercredi":
+//         alert("Tu as congé le mercredi")
+//         break;
+//     case "jeudi":
+//         alert("Tu as natation le jeudi")
+//         break;
+//     case "vendredi":
+//         alert("Tu as rdv au bar le vendredi")
+//         break;
+//     case "samedi":
+//         alert("Tu as une grosse sieste le samedi")
+//         break;
+//     case "dimanche":
+//         alert("Tu as athlétisme le dimanche")
+//         break;
+//     default:
+//         break;
+// }
+
+// EXO 2
+
+let age = parseInt(prompt("Quel est ton âge ?"))
+
+switch (true) {
+    case (age >= 18):
+        alert("Tu es majeur")
+        break;
+    default:
+        alert("Tu es mineur")
+        break;
+}
